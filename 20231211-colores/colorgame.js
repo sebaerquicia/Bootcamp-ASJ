9042c6da8705b955ef6406f7fbdaf2d74d7389b8
unknown <serquicia@petersen.corp>
2023-12-11 14:39:35 -0300

    Randomize de colores

--- 20231211-colores/colorgame.js
+++ 20231211-colores/colorgame.js
@@ -1,66 +1,68 @@
-let colors = ["rgb(255, 87, 51)","rgb(243, 255, 0)", "rgb(98, 124, 255)","rgb(255, 82, 242)", "rgb(51, 180, 47)", "rgb(162, 55, 231)"];
-
+/* let colors = [
+  "rgb(255, 87, 51)",
+  "rgb(243, 255, 0)",
+  "rgb(98, 124, 255)",
+  "rgb(255, 82, 242)",
+  "rgb(51, 180, 47)",
+  "rgb(162, 55, 231)",
+];
+ */
+let colors = new Array(6);
 const colorDisplay = document.getElementById("colorDisplay");
 const cuadrados = document.querySelectorAll(".square");
 const spanMsg = document.getElementById("message");
-const reset = document.getElementById("reset")
+const reset = document.getElementById("reset");
 const h1 = document.getElementById("h1");
-let pickedColor = colors[2];
-let colorsRGB =[]
-let clickedColor 
+let colorsRGB = [];
+/* let pickedColor = colors[2] */
+let pickedColor = colors[Math.floor(Math.random() * colors.length)];
+
+let clickedColor;
 
 colorDisplay.innerText = pickedColor;
-function principal(){
-
-    for(let i = 0; i<cuadrados.length; i++){
-        cuadrados[i].setAttribute("id", colors[i]);
-        cuadrados[i].setAttribute("onclick", `cambiarColor(${i})`);
-        cuadrados[i].setAttribute("style", `background-color:${colors[i]}`);
-        
-    }
+function principal() {
+  for (let i = 0; i < cuadrados.length; i++) {
+    cuadrados[i].setAttribute("id", colors[i]);
+    cuadrados[i].setAttribute("onclick", `cambiarColor(${i})`);
+    cuadrados[i].setAttribute("style", `background-color:${colors[i]}`);
+  }
 }
 
-function cambiarColor(i){
-    if(cuadrados[i].id == pickedColor){
-        spanMsg.innerText = "Correcto!";
-        cuadrados.forEach(cuadrado => {
-            cuadrado.style = `background-color:${pickedColor}`
-            h1.style = `background-color:${pickedColor}`
-        
-           
-        });
-
-    }else{
-        cuadrados[i].style = `background-color:#232323`;
-        spanMsg.innerText = "try Again";
-
-    }
-    
+function cambiarColor(i) {
+  if (cuadrados[i].id == pickedColor) {
+    spanMsg.innerText = "Correcto!";
+    cuadrados.forEach((cuadrado) => {
+      cuadrado.style = `background-color:${pickedColor}`;
+      h1.style = `background-color:${pickedColor}`;
+    });
+  } else {
+    cuadrados[i].style = `background-color:#232323`;
+    spanMsg.innerText = "try Again";
+  }
 }
 
-function randomColor(){
-    let red = Math.floor(Math.random() * 256)
-    let green = Math.floor(Math.random() * 256)
-    let blue = Math.floor(Math.random() * 256)
-    return `rgb(${red}, ${green}, ${blue})`
+function randomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
-
-function generateRandomColors(){
-    for(i=0; i < cuadrados.length; i++){
-        colorsRGB.push(randomColor())
-        colors = colorsRGB
-  
-    }
-    pickedColor = colors[(Math.floor(Math.random() * colors.length))]
-    colorDisplay.innerText = pickedColor;
+function generateRandomColors() {
+  for (i = 0; i < cuadrados.length; i++) {
+    colorsRGB.push(randomColor());
+    colors = colorsRGB;
+  }
+  pickedColor = colors[Math.floor(Math.random() * colors.length)];
+  colorDisplay.innerText = pickedColor;
 }
 
-function resetGame(){
-    colorsRGB=[]
-    spanMsg.innerText = "";
-    h1.style = `background-color: #232323`
-    generateRandomColors()
-    principal()
+function resetGame() {
+  colorsRGB = [];
+  spanMsg.innerText = "";
+  h1.style = `background-color: #232323`;
+  generateRandomColors();
+  principal();
 }
-principal()
+resetGame();
+principal();
